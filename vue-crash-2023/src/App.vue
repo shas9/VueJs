923c10aefb1f81e20515ab04e67852f4ef5ad251
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-22 01:41:44 +0600

    Tasks component and v-for loop

--- vue-crash-2023/src/App.vue
+++ vue-crash-2023/src/App.vue
@@ -1,16 +1,47 @@
 <template>
   <div class = "container">
     <Header title="Task Tracker"/> 
+    <Tasks :tasks = "tasks" />
 
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: "March 1st at 2:30pm",
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: "March 2nd at 1:30pm",
+        reminder: true
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: "March 3rd at 10:30am",
+        reminder: false
+      }
+    ]
   }
 }
 </script>
