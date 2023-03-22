5d9c6d96b14217c8a1d0f18d4530dfed7bf183c4
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-22 20:06:02 +0600

    Toggle Reminder

--- vue-crash-2023/src/components/Tasks.vue
+++ vue-crash-2023/src/components/Tasks.vue
@@ -1,6 +1,7 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <Task @delete-task= "$emit('delete-task', task.id)" 
+        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" 
+        @delete-task= "$emit('delete-task', task.id)" 
         :task="task" />
     </div>
 </template>
@@ -16,6 +17,6 @@ export default {
     components: {
         Task
     },
-    emits: ['delete-task']
+    emits: ['delete-task', 'toggle-reminder']
 }
 </script>
