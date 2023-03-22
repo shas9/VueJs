06ad6e4eb25262fdcecb4742826bc01eef3b53c5
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-23 00:36:13 +0600

    Refactoring to use with backend

--- vue-crash-2023/src/App.vue
+++ vue-crash-2023/src/App.vue
@@ -33,25 +33,63 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+    async addTask(task) {
+      const res = await fetch('api/tasks',{
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(task)
+        } 
+      )
+
+      const data = await res.json()
+      this.tasks = [...this.tasks, data]
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if(confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter(
-          (task) => task.id !== id
-        )
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        })
+        
+
+        if(res.status === 200) {
+          this.tasks = this.tasks.filter(
+          (task) => task.id !== id)
+        } else {
+          alert("Task couldn't be deleted")
+        }
+
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json()
+
       this.tasks = this.tasks.map(
         (task) => task.id === id ? {
-          ...task, reminder: !task.reminder
+          ...task, reminder: data.reminder
         } : task
       )
     },
     async fetchTasks() {
-      const res = await fetch('http://localhost:4500/tasks')
+      const res = await fetch('api/tasks/')
+      const data = await res.json()
+
+      return data
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`)
       const data = await res.json()
 
       return data
