a313450663535add929339ee3227c0ae7fdf97e7
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-22 19:09:18 +0600

    Emit Events

--- vue-crash-2023/src/components/Tasks.vue
+++ vue-crash-2023/src/components/Tasks.vue
@@ -1,6 +1,7 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <Task :task="task" />
+        <Task @delete-task= "$emit('delete-task', task.id)" 
+        :task="task" />
     </div>
 </template>
 
@@ -14,6 +15,7 @@ export default {
     },
     components: {
         Task
-    }
+    },
+    emits: ['delete-task']
 }
 </script>
