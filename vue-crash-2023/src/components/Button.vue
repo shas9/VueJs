5d83596cdd79de9c8959a86690f00acb2bc52cb4
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-22 21:16:31 +0600

    Toggle Form by clicking button

--- vue-crash-2023/src/components/Button.vue
+++ vue-crash-2023/src/components/Button.vue
@@ -17,7 +17,7 @@ export default {
     },
     methods: {
         onClick() {
-            console.log('click')
+            this.$emit('btn-click')
         }
     }
 }
--- vue-crash-2023/src/components/Header.vue
+++ vue-crash-2023/src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
     <header>
         <h1> {{ title }}</h1>
-        <Button text = "Add Task" color = "green"/>
+        <Button @btn-click="$emit('toggle-add-task')" 
+                :text = "showAddTask ? 'Close' : 'Add Task'" 
+                :color = "showAddTask ? 'red' : 'green'" />
     </header>
 </template>
 
@@ -13,7 +15,8 @@ export default {
         title: {
             type: String,
             default: "Hello World"
-        }
+        },
+        showAddTask: Boolean
     },
     components: {
         Button
