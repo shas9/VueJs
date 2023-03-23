c5f1540b56b984dfad344b95827a0f9a0dcbbf31
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-24 02:09:34 +0600

    NavBar created and integrated with HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <HelloWorld />
+  <v-app id="home">
+    <NavBar />
+    <v-container fluid>
+
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import NavBar from '../components/NavBar.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    NavBar,
   },
 });
 </script>
