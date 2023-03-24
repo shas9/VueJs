80210b36cfa580d49b1c6c577a580b3a3be9f744
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-25 00:17:43 +0600

    Head implemented

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,9 @@
   <v-app id="home">
     <NavBar />
     <v-container fluid>
+      <div class="head">
 
+      </div>
     </v-container>
   </v-app>
 </template>
@@ -25,5 +27,35 @@ export default defineComponent({
 <style scoped>
 .v-container{
   padding: 16px 0;
+}
+.head {
+  position: relative;
+  text-align: center;
+  padding: 12px;
+  margin-bottom: 6px;
+  height: 400px;
+  width: 100%;
+  color: white;
+}
+.head::before {
+  content: " ";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, 6deg);
+}
+
+.head::after{
+  content: " ";
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, -6deg);
 }
 </style>
