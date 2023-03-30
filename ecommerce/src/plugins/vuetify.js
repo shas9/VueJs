4f8cde2b474ef07143617835f1757fea849f7429
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-31 02:06:40 +0600

    Integrated plugin with buetify

--- ecommerce/src/plugins/vuetify.js
+++ ecommerce/src/plugins/vuetify.js
@@ -1,4 +1,6 @@
 // Styles
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
@@ -7,4 +9,16 @@ import { createVuetify } from 'vuetify'
 
 export default createVuetify(
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  {
+    icons: {
+      iconfont: 'md' || 'fa' || 'md'
+    },
+    theme: {
+      themes: {
+        light: {
+          primary: '#7C92FE' 
+        }
+      }
+    }
+  }
 )
