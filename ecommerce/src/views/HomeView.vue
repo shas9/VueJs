c7544a1a2c7d167c9ddd97239b87a3f206501e06
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-03-31 02:25:24 +0600

    Navbar, Footer, Dashboard integrated with App.vue and Helloworld is deleted

--- ecommerce/src/views/HomeView.vue
+++ ecommerce/src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
-  <HelloWorld />
+  <v-app>
+    <Navbar class = "index"/>
+    <Dashboard />
+    <Footer />
+  </v-app>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import Navbar from '../components/Navbar.vue';
+import Dashboard from '../components/Dashboard.vue';
+import Footer from '../components/Footer.vue';
+
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    Navbar,
+    Dashboard,
+    Footer
   },
 });
 </script>
+
+<style scoped>
+.index {
+  z-index: 9999;
+}
+</style>
